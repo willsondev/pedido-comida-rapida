945a66e1d10ac8560daf06809e20626862198a2f
willsondev <willsondev12@gmail.com>
2024-03-24 17:34:10 -0300

    tailwindcc incorporado

---
+++ pedido-comida/src/components/MainPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="main-page bg-black py-10">
+      <h1 class="text-2xl text-red-500 text-center">Bienvenido a nuestra página de pedidos de comida rápida</h1>
+      <!-- Aquí puedes agregar más contenido como imágenes, botones, etc. -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MainPage'
+  }
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
